ea137c654d3c06471aa2e996217f35d136dfb64c
lzy-Jerry <763626908@qq.com>
2023-01-05 23:23:23 +0800

    feat: 静态方法转换

--- src/utils/hackHeader.js
+++ src/utils/hackHeader.js
@@ -1,35 +1,42 @@
-const hackHeader = () => {
-  const isReplaceOrigin = true; // 是否替换origin
-  const isAddOrigin = true; // 是否添加origin
+const hackHeader = (url) => {
+  let isReplaceOrigin = true; // 是否替换origin
+  let isAddOrigin = true; // 是否添加origin
+  let refererValue = false;
 
 
   if (url.indexOf("://music.163.com/") !== -1) {
-    referer_value = "http://music.163.com/";
+    refererValue = "http://music.163.com/";
   }
   if (url.indexOf("://gist.githubusercontent.com/") !== -1) {
-    referer_value = "https://gist.githubusercontent.com/";
+    refererValue = "https://gist.githubusercontent.com/";
   }
 
   if (url.indexOf("c.y.qq.com/") !== -1 || (url.indexOf("i.y.qq.com/") !== -1)
         || (url.indexOf("qqmusic.qq.com/") !== -1)
         || (url.indexOf("music.qq.com/") !== -1)
         || (url.indexOf("imgcache.qq.com/") !== -1)) {
-    referer_value = "https://y.qq.com/";
+    refererValue = "https://y.qq.com/";
   }
 
   if (url.indexOf(".kugou.com/") !== -1) {
-    referer_value = "http://www.kugou.com/";
+    refererValue = "http://www.kugou.com/";
   }
 
   if (url.indexOf(".kuwo.cn/") !== -1) {
-    referer_value = "http://www.kuwo.cn/";
+    refererValue = "http://www.kuwo.cn/";
   }
 
   if (url.indexOf(".bilibili.com/") !== -1) {
-    referer_value = "http://www.bilibili.com/";
-    replace_origin = false;
-    add_origin = false;
+    refererValue = "http://www.bilibili.com/";
+    isReplaceOrigin = false;
+    isAddOrigin = false;
   }
+
+  return {
+    refererValue,
+    isReplaceOrigin,
+    isAddOrigin,
+  };
 };
 module.exports = {
   hackHeader,
